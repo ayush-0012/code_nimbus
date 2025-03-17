032059a4504c43f77f7580600f13b6cbd4a9c984
ayush-0012 <ayushbisht0012@gmail.com>
2025-03-17 16:02:17 +0530

    cors setup

--- server/index.ts
+++ server/index.ts
@@ -1,16 +1,29 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import { userRouter } from "./src/routes/user.routes";
+import cors from "cors";
 
 dotenv.config();
-
 const app: Express = express();
 
 const PORT = process.env.PORT;
+
+const corsOptions = {
+  origin: "*",
+  method: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+
+app.use("/api/user", userRouter);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("server is running, home page");
 });
 
-app.listen(4000, () => {
+app.listen(PORT, () => {
   console.log("server running on port", PORT);
 });
